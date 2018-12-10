0080b65384cded268ab4e1d0f3881b9579cfab12
Lise <lise.gourves@gmail.com>
2018-12-10 14:38:50 +0100

    Gestionnaire d'évènements sur le formulaire

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,3 +6,9 @@
 
  let title = document.getElementById('main-title');
  title.innerHTML = 'Hello Javascript';
+
+ // Créer une instance de Login : le mot clé new permet de créer une instance à partir de la classe login
+ const login = new Login();
+
+
+
---
+++ src/user/login.class.js
@@ -0,0 +1,46 @@
+class Login {
+    constructor() {
+        //Modifier le titre du document HTML
+        $(document).attr('title', 'Identification');
+
+        //Modifier le titre de la page
+        $('#main-title').html('Identifiez-vous');
+
+        // Définition du listener sur le formulaire
+        this.formListener();
+
+    }
+
+
+    /**
+     * formListener Gestionnaire d'évènements sur le formulaire Login (on appelle la méthode comme on veut, si possible en anglais ... 
+     * param void permet de dire que la méthode ne prend pas de paramètre et le return que la méthode ne retourne rien)
+     * @param void
+     * @return void
+     */
+    formListener() {
+        $('#loginForm').on(
+            'keyup',
+            // callback : fonction appelée si l'évènement défini survient
+            function(event) {
+                // Vérifier le contenu des champs
+                const login = $('[name=loginField]');
+                const password = $('[name=passwordField]');
+
+                // Est-ce que les deux champs sont remplis
+                if (login.val() !== '' && password.val() !== '') {
+                    // on peut activer le bouton connexion
+                    $('#btnLogin').removeAttr('disabled');
+                
+                } else {
+                    // Non, ça ne le fait pas tout seul, il faut lui dire
+                    $('#btnLogin').attr('disabled', 'disabled');
+                }
+
+            }
+        );
+
+
+    }
+}
+
